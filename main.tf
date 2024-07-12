e97319413caff85a6541a557d5c21f1e4b9097f2
Cristian Beraha <cristian@cloudops.works>
2024-07-12 17:18:53 -0300

    chore: rollback version

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ module "dns" {
 
 module "version" {
   source          = "cloudopsworks/beanstalk-version/aws"
-  version         = "1.0.11"
+  version         = "1.0.10"
   region          = var.region
   sts_assume_role = var.sts_assume_role
 
